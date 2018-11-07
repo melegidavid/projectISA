011b4ffce9056152b4171e1bc9d94fae75662e06
melegidavid <44850885+melegidavid@users.noreply.github.com>
2018-11-07 23:12:05 +0100

    Inicijalizacija projekta

---
+++ projectISA/src/main/java/ftn/isa/projectISA/ProjectIsaApplication.java
@@ -0,0 +1,12 @@
+package ftn.isa.projectISA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIsaApplication.class, args);
+	}
+}
